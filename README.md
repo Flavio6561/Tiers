623ba75a929b329a856f7798ad008c259f736070
flavio6561 <flaviosessantacinquesessantuno@gmail.com>
2025-04-05 15:23:45 +0200

    Version 0.3.0_1.20.3-1.20.4 Full Changelog: https://github.com/Flavio6561/Tiers/wiki/Version-changelogs

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Tiers branch for 1.20.1 - 1.20.2
+## Tiers branch for 1.20.3 - 1.20.4
 
 ### See [master](https://github.com/Flavio6561/Tiers)
 
--- src/client/java/com/tiers/mixin/client/ModifyNameTagsClientMixin.java
+++ src/client/java/com/tiers/mixin/client/ModifyNameTagsClientMixin.java
@@ -10,12 +10,12 @@ import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(PlayerEntity.class)
 public abstract class ModifyNameTagsClientMixin {
-	@Shadow public abstract String getEntityName();
+	@Shadow public abstract String getNameForScoreboard();
 
 	@ModifyReturnValue(at = @At("RETURN"), method = "getDisplayName")
 	private Text getDisplayName(Text originalNameText) {
 		if (TiersClient.toggleMod)
-			return TiersClient.getFullName(this.getEntityName(), originalNameText);
+			return TiersClient.getFullName(this.getNameForScoreboard(), originalNameText);
 		return originalNameText;
 	}
 }
